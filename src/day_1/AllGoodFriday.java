5d050693261cbe0b7a7bd9c50b19c852bac3eaa2
nurialis <nurialis34@gmail.com>
2020-04-24 14:51:32 -0500

    add AllGoodFriday.java

---
+++ src/day_1/AllGoodFriday.java
@@ -0,0 +1,9 @@
+package day_1;
+
+public class AllGoodFriday {
+    public static void main(String[] args) {
+        System.out.println("All Fridays are Good");
+
+        // goood to goo
+    }
+}
