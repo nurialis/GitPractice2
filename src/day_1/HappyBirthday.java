91f28d8c7c0aa195a283df30141518e3b75a3055
nurialis <64111847+nurialis@users.noreply.github.com>
2020-04-28 14:52:09 -0500

    Update HappyBirthday.java

--- src/day_1/HappyBirthday.java
+++ src/day_1/HappyBirthday.java
@@ -2,7 +2,10 @@ package day_1;
 
 public class HappyBirthday {
     public static void main(String[] args) {
-        System.out.println("Happy Birthday all");
+        System.out.println("Happy Birthday all and how are you");
+        
+        // this line is added from git hub
+        
 
     }
 }
