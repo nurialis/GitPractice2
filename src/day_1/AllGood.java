19665243579e10f80285d675b45deac609268bff
nurialis <nurialis34@gmail.com>
2020-04-28 14:39:38 -0500

    add HappyBirtday.java

--- src/day_1/AllGood.java
+++ src/day_1/AllGood.java
@@ -3,8 +3,11 @@ package day_1;
 public class AllGood {
     public static void main(String[] args) {
         System.out.println("All Goood");
+        System.out.println("Merhaba");
+        System.out.println("Adding new stafgit");
 
          // I hope everything is GOOD
         // ok we are coming again
+        // day 1 and day 2 together
     }
 }
